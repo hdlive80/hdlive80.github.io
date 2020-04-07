7a1e24eb0e9565ef555189065ca5d36f711a612b
https://github.com/hdlive80/hdlive80.github.io.git <59584882+hdlive80@users.noreply.github.com>
2020-04-06 23:02:27 -0500

    commit final draft

--- lesson-thirteen/java/guides.java
+++ lesson-thirteen/java/guides.java
@@ -1,22 +1,22 @@
-const requestURL = '';
+const requestURL = 'C:\Users\Livesay\Documents\GitHub\hdlive80.github.io\lesson-thirteen\json\guides.json';
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    const prophets = jsonObject['prophets'];
+    const prophets = jsonObject['guides'];
     console.table(jsonObject); // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++) {
+    for (let i = 0; i < guides.length; i++) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let div1 = document.createElement('div');
       let div2 = document.createElement('div');
       let image = document.createElement('img');
 
-      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      div1.textContent = 'Birth date: ' + prophets[i].birthdate;
-      div2.textContent = 'Birth Place: ' + prophets[i].birthplace;
+      h2.textContent = guides[i].name+ ' ' + guides[i].lastname;
+      div1.textContent = 'Certification Level: ' + guides[i].certlevel;
+      div2.textContent = 'Years of Experience: ' + guides[i].yearexp;
       image.setAttribute('src', prophets[i].imageurl);
       image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order)
 
@@ -28,4 +28,3 @@ fetch(requestURL)
       document.querySelector('div.cards').appendChild(card);
     }
   });
-
